03a83ad5e05d2c40f310184c1e89d5cf456089b3
Greg <grishaturilov@gmail.com>
2023-11-06 16:35:44 +0300

    refactor

--- client/src/features/todos/TodosPage.tsx
+++ client/src/features/todos/TodosPage.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-// import './Games.scss';
 import type { RootState } from '../../redux/store';
 import TodoItem from './TodoItem';
 import AddTodoForm from './AddTodoForm';
-// import Date from './Date';
 
 const TodosPage = (): JSX.Element => {
   const [filter, setFilter] = useState('all');
-  // const user = useSelector((store: RootState) => store.auth.auth);
-  // const [prior, setPrior] = useState('all');
   const todoss = useSelector((store: RootState) => store.todos.todos);
   const sortedTodos = todoss.sort((todoA, todoB) => todoB.level_id - todoA.level_id);
   const todos = useSelector((store: RootState) => store.todos.todos);
@@ -17,15 +13,8 @@ const TodosPage = (): JSX.Element => {
     setFilter(status);
   };
 
-  // const handlePrior = (level): void => {
-  //   setPrior(level);
-  // };
-
   const filteredTodos =
     filter === 'all' ? todos : todos.filter((todo) => todo.status === (filter === 'completed'));
-
-  // const sortedPrior =
-  //   prior === 'all' ? todos : todos.filter((todo) => todo.level_id === (prior === '1'));
 
   return (
     <div className="todo__container">
@@ -46,14 +35,9 @@ const TodosPage = (): JSX.Element => {
       {sortedTodos.map((todo) => (
         <div key={todo.id} />
       ))}
-      {/* {user && user.id === todos.user_id ? (
-        <> */}
       {filteredTodos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
-      {/* <Date /> */}
-      {/* </>
-      ) : null} */}
     </div>
   );
 };
