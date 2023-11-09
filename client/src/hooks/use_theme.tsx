4d3530c5c6445ebabcd9f313657d6c4c1d650e27
NovikovD07 <daniilan1993@gmail.com>
2023-11-09 19:58:16 +0300

    css

--- client/src/hooks/use_theme.tsx
+++ client/src/hooks/use_theme.tsx
@@ -1,9 +1,10 @@
 import { useLayoutEffect, useState } from 'react';
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(localStorage.getItem('app-theme')|| 'light');
   useLayoutEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('app-theme', theme)
   }, [theme]);
   return {
     theme,
